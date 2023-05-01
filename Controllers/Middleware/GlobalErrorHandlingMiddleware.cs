e2a37f7d202d140be762aca1fdb81a685616a95b
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2023-05-01 15:06:49 -0300

    Criação do middleware para captura de exceções globais

---
+++ Controllers/Middleware/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Projeto_Nemo.Controllers.Middleware;
+
+public class GlobalErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    
+    public GlobalErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await handleExceptionAsync(context, ex);
+        }
+        
+        
+    }
+
+    private static Task handleExceptionAsync(HttpContext context, Exception exception)
+    {
+        HttpStatusCode status;
+        // TODO: Logar o stackTrace da aplicação
+        string stackTrace = String.Empty;
+        string mensagem;
+        var exceptionType = exception.GetType();
+
+        // TODO: Adicionar o restante das exceções aqui
+        
+        // Trata default
+        mensagem = exception.Message;
+        status = HttpStatusCode.InternalServerError;
+
+        var response = JsonSerializer.Serialize(new { mensagem, status });
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int) status;
+        return context.Response.WriteAsync(response);
+    }
+}
