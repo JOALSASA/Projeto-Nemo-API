f3e0683d033c3027dd600052cc4e35d3506bb10e
Vitor Gabriel Silva Nascimento <dev.vitor99@gmail.com>
2023-07-12 22:51:24 -0300

    Bug no retorno do usuario nos aquarios que o usuario logado tem permissao

--- Repositories/AquarioRepository.cs
+++ Repositories/AquarioRepository.cs
@@ -34,11 +34,35 @@ namespace Projeto_Nemo.Repositories
 
         public List<Aquario> RecuperarPorUsuarioId(int id, string? nomeAquario)
         {
+
+            //Uma lista com os aquarios do usuario logado
+            var aquariosProprietario = _dbContext.Aquarios.Where(u => u.Usuario.Id == id).ToList();
+
+            //Uma lista com os aquarios que o usuario logado tem permissao
+            var aquariosComPermissao = _dbContext.UsuarioAquarios
+                .Where(u => u.UsuariosId == id)
+                .Include(u => u.Usuario)
+                .Select(u => u.Aquario)
+                .ToList();
+
+            //Juncao das duas listas
+            var aquarios = aquariosProprietario.Concat(aquariosComPermissao).ToList();
+
+            if (nomeAquario != null)
+            {
+                return aquarios.Where(u => u.Usuario.Id == id & u.Nome.Contains(nomeAquario)).ToList();
+            }
+
+            return aquarios;
+
+
+            /*
             if (nomeAquario != null)
             {
                 return _dbContext.Aquarios.Where(u => u.Usuario.Id == id & u.Nome.Contains(nomeAquario)).ToList();
             }
             return _dbContext.Aquarios.Where(u => u.Usuario.Id == id).ToList();
+            */
         }
 
         public Aquario Inserir(Aquario aquario)
