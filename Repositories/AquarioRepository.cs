f2ba61aecf040b588889f99671e041172a8529bc
Vitor Gabriel Silva Nascimento <dev.vitor99@gmail.com>
2023-07-12 23:58:37 -0300

    Ajustado o bug que nao retornava o usuario para aquarios em que o usuario logado possui permissao

--- Repositories/AquarioRepository.cs
+++ Repositories/AquarioRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Projeto_Nemo.Data;
 using Projeto_Nemo.Models;
+using Projeto_Nemo.Models.Dto;
 using Projeto_Nemo.Repositories.Interfaces;
 
 namespace Projeto_Nemo.Repositories
@@ -35,34 +36,22 @@ namespace Projeto_Nemo.Repositories
         public List<Aquario> RecuperarPorUsuarioId(int id, string? nomeAquario)
         {
 
-            //Uma lista com os aquarios do usuario logado
-            var aquariosProprietario = _dbContext.Aquarios.Where(u => u.Usuario.Id == id).ToList();
-
-            //Uma lista com os aquarios que o usuario logado tem permissao
-            var aquariosComPermissao = _dbContext.UsuarioAquarios
-                .Where(u => u.UsuariosId == id)
-                .Include(u => u.Usuario)
-                .Select(u => u.Aquario)
-                .ToList();
-
-            //Juncao das duas listas
-            var aquarios = aquariosProprietario.Concat(aquariosComPermissao).ToList();
-
-            if (nomeAquario != null)
+            if (nomeAquario != null) 
             {
-                return aquarios.Where(u => u.Usuario.Id == id & u.Nome.Contains(nomeAquario)).ToList();
+                return _dbContext.Aquarios
+                    .Include(a => a.UsuarioAquarios
+                        .Where(ua => ua.UsuariosId == id))
+                    .Include(a => a.Usuario)
+                        .Where(a => a.Usuario.Id == id && a.Nome.Contains(nomeAquario))
+                    .ToList();
+
             }
 
-            return aquarios;
-
-
-            /*
-            if (nomeAquario != null)
-            {
-                return _dbContext.Aquarios.Where(u => u.Usuario.Id == id & u.Nome.Contains(nomeAquario)).ToList();
-            }
-            return _dbContext.Aquarios.Where(u => u.Usuario.Id == id).ToList();
-            */
+            return _dbContext.Aquarios
+                    .Include(a => a.Usuario)
+                    .Include(a => a.UsuarioAquarios)
+                        .Where(a => a.Usuario.Id == id || a.UsuarioAquarios.Any(ua => ua.UsuariosId == id))
+                    .ToList();
         }
 
         public Aquario Inserir(Aquario aquario)
